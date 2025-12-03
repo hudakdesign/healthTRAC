fe923f4d4c0c77c6638a9e4be2c501401599e71a
JamesCDavenport <jcdavenp@uvm.edu>
2025-12-02 20:58:20 -0500

    implemented basic audio recording for testing purposes in prototype

--- satellite-server/voice_recorder.py
+++ satellite-server/voice_recorder.py
@@ -1,12 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor
 import time
 import sounddevice as sd
 from scipy.io.wavfile import write
 # import wavio as wv
 
 SAVE_PATH = "data/"
-
 FREQ = 44100 # Sample frequency
 DURATION = 5 # Duration in seconds
+
+recordings = {}
 
 def create_recording(duration):
     recording = sd.rec(int(duration * FREQ),
@@ -16,10 +18,28 @@ def create_recording(duration):
 
     return recording
 
+def save_recording(recording):
+    write(f"{SAVE_PATH}{time.time()}.wav", FREQ, recording)
+
 if __name__ == "__main__":
-    recording_index = 0
+    # recording_index = 0
+    # while True:
+    #     curr_recording = create_recording(DURATION)
+    #     write(f"{SAVE_PATH}{time.time()}.wav", FREQ, curr_recording)
+    #     recording_index += 1
+
+    # record_thread = threading.Thread(target=)
+
+    # save_thread
+
+
     while True:
-        curr_recording = create_recording(DURATION)
-        write(f"{SAVE_PATH}{time.time()}.wav", FREQ, curr_recording)
-        recording_index += 1
-    
+        try:
+            recordings[time.time()] = create_recording(DURATION)
+        except KeyboardInterrupt:
+            print(recordings.keys())
+            raise SystemExit
+
+    # while True:
+    #     try:
+    #         executor.submit()
