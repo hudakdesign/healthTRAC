d453331dac994121e232f73c7962183218306a06
JamesCDavenport <jcdavenp@uvm.edu>
2026-02-02 13:44:20 -0500

    Added code for checking if timestamp is within threshold for stellite recording

--- satellite_recording.py
+++ satellite_recording.py
@@ -4,25 +4,41 @@ import os
 
 DEBUG = True
 RECORDING_FILE = "recording_timestamp"
-# THRESHOLD = In nanoseconds
+THRESHOLD = 10e9 # In nanoseconds
 check_recording_command = f"cat {RECORDING_FILE}"
 recording_timestamp = 0
 
+running = True
+
 # Check the recording flag value on the hub
-def get_recording_timestamp():
-    # TESTING CODE
-    if DEBUG:
-        recording_timestamp = int(os.popen(check_recording_command).read())
+# Set global recording timestamp value
+def set_recording_timestamp():
+    global recording_timestamp
+    recording_timestamp = int(os.popen(check_recording_command).read())
         
-
-
 # Check if stored recording flag value is within the threshold
 def check_if_in_threshold():
     curr_time = time.time_ns
-    if (curr_time < (recording_timestamp + THR )
+    if (curr_time < (recording_timestamp + THRESHOLD )):
+        return True
+    return False
+
+def set_recording_timestamp_loop():
+    while running:
+        set_recording_timestamp()
+
+def test():
+    while running:
+        print(f"Current timestamp: {time.time_ns()}\nHub timestamp: {recording_timestamp}\nIn threshold: {check_if_in_threshold()}")
 
 def main():
-    get_recording_timestamp()
+    server_threads = []
+    if DEBUG:
+        server_threads.append(threading.Thread(test))
+    server_threads.append(threading.Thread(set_recording_timestamp_loop))
+    
+    for thread in server_threads:
+        thread.start()
 
 if __name__ == "__main__":
     main()
