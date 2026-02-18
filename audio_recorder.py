40f3bb9919058989673dbbc25f6f3059a0b13b80
JamesCDavenport <jcdavenp@uvm.edu>
2026-02-18 18:31:51 -0500

    Updated recorder

--- audio_recorder.py
+++ audio_recorder.py
@@ -1,12 +1,13 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 import wavio as wv
+import sys
 
 # Defaults
 channels = 2
 duration = 5
 dtype = 'int16'
-freq = 44100
+sample_rate = 44100
 file_name = "recording.wav"
 file_directory = "data/"
 file_path = f"{file_directory}{file_name}"
@@ -16,8 +17,20 @@ audio_frames = []
 recording = True
 
 
+def callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    audio_frames.append(indata.copy())
 
-
+def start_recording():
+    # Start recording stream
+    with sd.InputStream(samplerate=sample_rate,
+                        channels=channels,
+                        dtype=dtype,
+                        callback=callback):
+        global recording
+        while recording:
+            sd.sleep(100)
 
 if __name__ == "__main__":
     # sample recording
