1b73a471f3656fad7a21a4a1cd427286634abd76
JamesCDavenport <jcdavenp@uvm.edu>
2026-03-07 09:57:07 -0500

    Created api for getting recording status from satellite

---
+++ satellite/recording_status.py
@@ -0,0 +1,22 @@
+import random
+import time
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    recording_data = get_recording_data()
+    return json.dumps(recording_data)
+
+# returns dictionary of recording debug info
+def get_recording_data():
+    dummy_data = {"time_ns": time.time_ns(),
+                  "channels": 2,
+                  "frequency": 41000,
+                  "amplitude": random.random()}
+    return dummy_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
