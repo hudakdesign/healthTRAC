1ba5eb1170084b66787ddc06959a2d84cfb00a71
JamesCDavenport <jcdavenp@uvm.edu>
2026-03-21 00:00:54 -0400

    Added placeholder code for the api

--- dashboard-subsystem/dashboard.py
+++ dashboard-subsystem/dashboard.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import json
+import random
 import threading
 
 import constants as c
@@ -11,7 +13,30 @@ def main():
     @app.route("/")
     def index():
         return render_template("dashboard.html")
+
+    @app.route("/imu")
+    def imu_api():
+        # Example of preparing data to send
+        x_vals = [0, 1, 2, 3, 4]
+        y_vals = [0, 1, 4, 9, 16]
+        
+
+        return json.dumps({
+            "x_vals": x_vals,
+            "y_vals": y_vals
+        })
     
+
+    @app.route("/fsr")
+    def fsr_api():
+        # Example of preparing data to send
+        x_vals = [0, 1, 2, 3, 4]
+        y_vals = [0, 8, 16, 8, 0]
+
+        return json.dumps({
+            "x_vals": x_vals,
+            "y_vals": y_vals
+        })
 
     # Starts server
     app.run(host="0.0.0.0", port=5000, debug=c.DEBUG)
