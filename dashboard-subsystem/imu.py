d6985f245217aa14ba23ff11c022df8e76822bf0
JamesCDavenport <jcdavenp@uvm.edu>
2026-04-10 10:50:15 -0400

    updated imu api to include all 3 accel values

--- dashboard-subsystem/imu.py
+++ dashboard-subsystem/imu.py
@@ -35,39 +35,14 @@ def get_imu_data():
 
         return x_vals, y_data
 
-
-# @app.route("/imu")
-# def imu_api():
-#     with open(DATA_PATH, mode = "r") as file:
-#         csv_dict_reader = csv.DictReader(file)
-
-#         data_dict = {}
-#         for idx, line in enumerate(csv_dict_reader):
-#             if idx == 0:
-#                 for key in line.keys():
-#                     data_dict[key] = [line[key]]
-#             else:
-#                 for key in line.keys():
-#                     data_dict[key].append(line[key])
-                
-#             if idx == 9:
-#                 break
-
-#         print(data_dict)
-
-#         output_dict = {
-#             "timestamp_ns": data_dict["timestamp_ns"],
-#             "accel_x": data_dict["accel_x"]}
-#         return json.dumps(output_dict)
-
 @app.route("/imu")
 def imu_api():
     # Example of preparing data to send
-    x_vals = [0, 1, 2, 3, 4]
-    y_data = [
-        [0, 1, 2, 3, 4],
-        [4, 3, 2, 1, 0]
-    ]
+    # x_vals = [0, 1, 2, 3, 4]
+    # y_data = [
+    #     [0, 1, 2, 3, 4],
+    #     [4, 3, 2, 1, 0]
+    # ]
 
     x_vals, y_data = get_imu_data()
 
@@ -85,6 +60,13 @@ def imu_api():
             "backgroundColor": "rgba(255,0,0,1.0)",
             "borderColor": "rgba(255,0,0,0.1)",
             "data": y_data[1]
+        },
+        {
+            "fill": False,
+            "lineTension": 0,
+            "backgroundColor": "rgba(0,255,0,1.0)",
+            "borderColor": "rgba(0,255,0,0.1)",
+            "data": y_data[2]
         }
     ]
 
