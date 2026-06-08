2f59c331e658270dd80bdccf668eca99c1798dd6
JamesCDavenport <jcdavenp@uvm.edu>
2026-06-08 14:27:27 -0400

    updated constants for testing

--- dashboard-subsystem/constants.py
+++ dashboard-subsystem/constants.py
@@ -2,7 +2,7 @@ DEBUG = True
 PORT = 8080
 MAX_CSV_AGE = 1 * 60 * 60
 
-fsr_url = "http://127.0.0.1:8080/debug/simulated_fsr"
+# fsr_url = "http://127.0.0.1:8080/debug/simulated_fsr"
 fsr_url = "http://127.0.0.1:8081/data"
-imu_url = "http://127.0.0.1:8080/debug/simulated_imu"
+# imu_url = "http://127.0.0.1:8080/debug/simulated_imu"
 imu_url = "http://127.0.0.1:8082/data"
