80d25ea3f30ab5f41ed3d52c9b911c7bdddaf2c9
JamesCDavenport <jcdavenp@uvm.edu>
2026-07-07 11:30:59 -0400

    Updated to complete basic setup process

--- firmware/IMU-Firmware/src/main.cpp
+++ firmware/IMU-Firmware/src/main.cpp
@@ -2,27 +2,64 @@
 
 #include "LSM6DS3.h"
 
-// Create instance of imu class
-LSM6DS3 myIMU(I2C_MODE, 0x6A);
+// Constants:
+static const int NUM_ACCELERATION_VALUES = 3;
+
+// polling constants
+static const int POLLING_RATE = 100;    // hz
+static const int POLLING_TIME_MINS = 5; // how long we need to collect data
+
+static const int POLLING_TIME_MS = POLLING_TIME_MINS * 60 * 1000;       // converted to ms
+static const int MS_BETWEEN_POLLS = 1000 / POLLING_RATE;                // how long between polls
+static const int MAX_STORED_POLLS = POLLING_TIME_MS / MS_BETWEEN_POLLS; // polling time / how long between polls
+
+// Struct declarations:
+struct imuDataPoll
+{
+  int timestamp;
+  float accelerationValues[NUM_ACCELERATION_VALUES];
+};
+
+// Globals:
+// imu class instance
+static LSM6DS3 myIMU(I2C_MODE, 0x6A);
+
+// data storage
+static imuDataPoll pollBuffer[MAX_STORED_POLLS];
 
 // put function declarations here:
-int plotImuData();
+void plotImuData();
 
-void setup() {
+void setup()
+{
   // Initialize serial
   Serial.begin(115200);
 
-  // give some time to initialize
+  // wait for serial to initialize
   delay(2000);
+
+  // configure imu
+  if (myIMU.begin() != 0)
+  {
+    Serial.println("Device error");
+  }
+  else
+  {
+    Serial.println("Device OK!");
+  }
+
+  // set pin modes
+  pinMode(LED_RED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
   plotImuData();
 }
 
 // put function definitions here:
-int plotImuData() {
+void plotImuData()
+{
   static float currentXAccel, currentYAccel, currentZAccel = 0;
 
   currentXAccel = myIMU.readFloatAccelX();
