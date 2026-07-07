9938622e9daec09ab89dda8ad9a79be8b40eed46
JamesCDavenport <jcdavenp@uvm.edu>
2026-07-07 10:39:01 -0400

    Added barebones plotting for accel data

--- firmware/IMU-Firmware/src/main.cpp
+++ firmware/IMU-Firmware/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
+#include "LSM6DS3.h"
+
+// Create instance of imu class
+LSM6DS3 myIMU(I2C_MODE, 0x6A);
+
 // put function declarations here:
-int myFunction(int, int);
+int plotImuData();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialize serial
+  Serial.begin(115200);
+
+  // give some time to initialize
+  delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  plotImuData();
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int plotImuData() {
+  static float currentXAccel, currentYAccel, currentZAccel = 0;
+
+  currentXAccel = myIMU.readFloatAccelX();
+  currentYAccel = myIMU.readFloatAccelY();
+  currentZAccel = myIMU.readFloatAccelZ();
+
+  Serial.println(">x_accel:" + (String)currentXAccel);
+  Serial.println(">y_accel:" + (String)currentYAccel);
+  Serial.println(">z_accel:" + (String)currentZAccel);
 }
