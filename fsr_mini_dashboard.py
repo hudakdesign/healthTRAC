47cf19ea57947e2b48b71ea8a97a33170db6b35a
JamesCDavenport <jcdavenp@uvm.edu>
2026-07-10 10:25:45 -0400

    implemented rolling buffer for graph data

--- fsr_mini_dashboard.py
+++ fsr_mini_dashboard.py
@@ -2,21 +2,25 @@ from flask import Flask, render_template
 import threading
 import queue
 import requests
-import json
 import time
 import sqlite3
 import subprocess
 import collections
+import pandas
 
 # Constants
 MAX_QUEUE_LEN = 5000
 FSR_URL = "http://10.0.1.27/"
-TIME_BETWEEN_POLLS = 5  # seconds
+TIME_BETWEEN_POLLS = 0.5  # seconds
 DATABASE_FILENAME = "data/test.db"
 
 # Globals
 running = True
 fsr_data_queue = queue.Queue(maxsize=MAX_QUEUE_LEN)
+
+# rolling buffer for storing recent values
+fsr_data_buffer = collections.deque(maxlen=MAX_QUEUE_LEN) # TODO: change this to a better value
+fsr_data_buffer_lock = threading.Lock()
 
 # Architecture:
 # Threads:
@@ -61,21 +65,19 @@ def get_fsr_data():
         # turn json from request into dict
         data = r.json()
 
-        timer = time.time()
+        # timer = time.time()
         # turn this data into individual polls and put into queue
         queue_individual_polls(data)
         # print how much time queueing these took
-        print(f"Time to queue: {time.time() - timer}")
+        # print(f"Time to queue: {time.time() - timer}")
 
         # waits until its time to poll again
         time.sleep(TIME_BETWEEN_POLLS)
 
 
 # consumer:
-#   1. takes data out of the queue, saves it to the db TODO
-#   2. next load data into a pandas df TODO
-#   3. extract aggregate data from this df (averages for every half second maybe...) TODO
-#   4. push aggregate data to rolling buffer TODO
+#   1. takes data out of the queue, saves it to the db DONE
+#   2. next push the data to a rolling buffer DONE
 def process_fsr_data():
     def add_fsr_data(conn, fsr_data):
         sql = '''INSERT INTO fsr_one(timestamp, sensor0, sensor1, sensor2, sensor3, sensor4, sensor5, sensor6, sensor7)
@@ -107,13 +109,39 @@ def process_fsr_data():
             # add to db
             add_fsr_data(conn, fsr_data)
 
-            # print(f"Data poll: {new_data_poll}")
-            # print(f"Data poll -> db compatible {fsr_data}")
+            # push to rolling buffer
+            with fsr_data_buffer_lock:
+                fsr_data_buffer.append(new_data_poll)
 
 
 # webserver: TODO
 def serve_flask_app():
     pass
+
+# utility task
+# periodically display buffer contents
+def periodically_display_buffer_contents():
+    while running:
+        # lock the buffer
+        with fsr_data_buffer_lock:
+            # print out the last (most recent) value
+            # if there is data in the buffer
+            if (len(fsr_data_buffer) > 0):
+                print(fsr_data_buffer[-1])
+        
+        # wait to avoid flooding the terminal
+        time.sleep(1)
+
+# small scale prototype
+# task gets data out of rolling buffer and
+# loads it into a pandas df
+# then it prints out averages based on the data
+def get_aggregate_data():
+    pass
+    # while running:
+        # lock the buffer
+
+
 
 def create_database():
     # create data directory
@@ -147,15 +175,17 @@ def main():
     # create threads
     get_fsr_data_thread = threading.Thread(target=get_fsr_data)
     process_fsr_data_thread = threading.Thread(target=process_fsr_data)
+    display_contents_thread = threading.Thread(target=periodically_display_buffer_contents)
+
 
     # start threads
     get_fsr_data_thread.start()
     process_fsr_data_thread.start()
+    display_contents_thread.start()
 
     # stop if enter is pressed
     input("")
     running = False
-
 
 
 if __name__ == "__main__":
