aa0b0847647accd5f5dab4ca29f8f6e5f2fd463f
JamesCDavenport <jcdavenp@uvm.edu>
2026-08-07 12:27:08 -0400

    Created setup code

--- firmware/IMU-XIAO/src/main.cpp
+++ firmware/IMU-XIAO/src/main.cpp
@@ -4,8 +4,10 @@
 #include <DataPoll.h>
 
 // Constants
+const char *DEVICE_NAME = "IMU-XIAO";
 const char *SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 const char *CHARACTERISTIC_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+const int8_t POWER_LEVEL = 8;
 
 // Globals
 NimBLEStreamServer bleStream;
@@ -43,12 +45,14 @@ class ServerCallbacks : public NimBLEServerCallbacks
     pServer->updateConnParams(connInfo.getConnHandle(), 12, 24, 0, 200);
   }
 
-  void onDisconnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo, int reason) override {
+  void onDisconnect(NimBLEServer *pServer, NimBLEConnInfo &connInfo, int reason) override
+  {
     Serial.printf("Client disconnected: (reason: %d) restarting advertising\n", reason);
     NimBLEDevice::startAdvertising();
   }
 
-  void onMTUChange(uint16_t MTU, NimBLEConnInfo &connInfo) override {
+  void onMTUChange(uint16_t MTU, NimBLEConnInfo &connInfo) override
+  {
     Serial.printf("MTU updated: %u for connection ID: %u\n", MTU, connInfo.getConnHandle());
   }
 
@@ -56,19 +60,65 @@ class ServerCallbacks : public NimBLEServerCallbacks
 
 void setup()
 {
+  int errorCount = 0;
+
   // initialize serial
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("IMU-XIAO Setup");
 
   // initialize imu
+  if (myImu.begin() == 0)
+  {
+    Serial.println("Initialize IMU: success");
+  }
+  else
+  {
+    Serial.println("Initialize IMU: ERROR");
+    errorCount++;
+  }
 
   // initialize NimBLE
+  NimBLEDevice::init(DEVICE_NAME);
 
   // set power level
+  if (NimBLEDevice::setPower(POWER_LEVEL))
+  {
+    Serial.println("Set Power Level: success");
+  }
+  else
+  {
+    Serial.println("Set Power Level: ERROR");
+    errorCount++;
+  }
 
   // create ble server and set its callbacks
+  NimBLEServer *pServer = NimBLEDevice::createServer();
+  pServer->setCallbacks(&serverCallbacks);
 
   // intialize stream server and set overflow callback
+  if (bleStream.begin(NimBLEUUID(SERVICE_UUID),
+                      NimBLEUUID(CHARACTERISTIC_UUID),
+                      1024,
+                      1024,
+                      false))
+  {
+    Serial.println("Create BLE Stream: success");
+  }
+  else
+  {
+    Serial.println("Create BLE Stream: ERROR");
+    errorCount++;
+  }
 
   // create advertising instance
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setName(DEVICE_NAME);
+  pAdvertising->enableScanResponse(true);
+  pAdvertising->start();
+
+  Serial.printf("Setup completed with %d detected errors\n", errorCount);
 }
 
 void loop()
