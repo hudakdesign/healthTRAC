4302628274f1806f6f025daa793fd6c5433ce6b3
JamesCDavenport <jcdavenp@uvm.edu>
2026-09-04 09:52:10 -0400

    Added method for updating raw data to `sensor_subsystems.py`

--- sensor_subsystems.py
+++ sensor_subsystems.py
@@ -4,6 +4,7 @@
 import collections
 
 import requests
+import pandas as pd
 
 # Constants:
 DATA_POLL_QUEUE_LENGTH = 1000
@@ -16,7 +17,7 @@ class Sensor_Subsystem:
 
     Attributes:
         subsystem_url: The URL for accessing the microphone array.
-        raw_data_polls: A list for containing received raw data polls.
+        raw_data_polls: A dataframe for containing received raw data polls.
         aggregate_data_polls_short: A ring buffer of aggregate datapoints
             used for display and debugging on the dashboard.
         aggregate_data_polls_long: A ring buffer of very low frequency
@@ -27,7 +28,7 @@ class Sensor_Subsystem:
 
     def __init__(self, subsystem_url):
         self.subsystem_url = subsystem_url
-        self.raw_data_polls = []
+        self.raw_data_polls = None
         self.aggregate_data_polls_short = collections.deque(
             maxlen=DATA_POLL_QUEUE_LENGTH
         )
@@ -44,7 +45,7 @@ class Sensor_Subsystem:
         `is_connected` is set to `False` and returns `False`; otherwise, it is
         set to `True`.
         """
-        
+
         try:
             # tries to get the data from the `subsystem_url`
             response = requests.get(self.subsystem_url, timeout=REQUEST_TIMEOUT_SECONDS)
@@ -53,26 +54,37 @@ class Sensor_Subsystem:
             self.is_connected = False
             # and return `False`
             return False
-        # if the response came though then 
+        # if the response came though then
         self.is_connected = True
         return response.json()
 
     def update_raw_data(self):
         """Updates `raw_data_polls` with response data from subsystem
-        
+
         Polls the subsystem, takes the dictionary and uses it to update
         `raw_data_polls`. Returns `True` if successful, and `False` if anything
         went wrong.
         """
-        
+
         # poll the subsystem
         response = self._poll_subsystem()
-        
-        # check if a response came through (a dict was received)
-        if response is dict:
-            pass
-        
-        
+
+        # check that the poll was successful
+        if response:
+            incoming_raw_data = pd.DataFrame(response["dataPolls"])
+
+            # updates the dataframe with the incoming data
+            # special case for if this is the first poll
+            if self.raw_data_polls == None:
+                self.raw_data_polls = incoming_raw_data
+            else:
+                self.raw_data_polls = pd.concat(
+                    [self.raw_data_polls, incoming_raw_data], ignore_index=True
+                )
+            return True
+        else:
+            return False
+
 
 class Microphone_Array(Sensor_Subsystem):
     """Handles retrieving data from microphone array subsystems
