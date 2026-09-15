f7fe27f82e1d44aa80a5f9e104e51145810c44a1
JamesCDavenport <jcdavenp@uvm.edu>
2026-09-15 15:06:39 -0400

    Once again attempted fixing bug with database saving

--- sensor_subsystems.py
+++ sensor_subsystems.py
@@ -90,9 +90,10 @@ class Sensor_Subsystem:
         if self.con is None:
             self.con = sqlite3.connect(f"data/{self.database_name}")
 
-        poll_df["receiveTime"] = self.last_receive_time // 1e6  # rx time ms
+        poll_df = poll_df.insert(
+            -1, "receiveTime", self.last_receive_time // 1e6
+        )  # rx time ms
         poll_df.to_sql(name="data_polls", con=self.con, if_exists="append")
-        poll_df = poll_df.drop(columns=["receiveTime"])
 
     def _update_aggregate_data(self):
         """Updates aggregate data buffers using data from `raw_data_polls`
